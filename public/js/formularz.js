5640b6a5d98e7806c1cf0ec39f0e17ca7a44eb6c
SavanDalage <nekomimiwolf@gmail.com>
2024-07-01 19:56:01 +0200

    formularz.js changes to fetch

--- public/js/formularz.js
+++ public/js/formularz.js
@@ -1,50 +1,41 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  const form = document.getElementById("training-form");
-  const resetBtn = document.getElementById("reset-btn");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-  resetBtn.addEventListener("click", () => {
-    form.reset();
-  });
+  const formData = new FormData(form);
+  const data = {};
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(form);
-    const data = {};
-
-    formData.forEach((value, key) => {
-      if (data[key]) {
-        if (!Array.isArray(data[key])) {
-          data[key] = [data[key]];
-        }
-        data[key].push(value);
-      } else {
-        data[key] = value;
+  formData.forEach((value, key) => {
+    if (data[key]) {
+      if (!Array.isArray(data[key])) {
+        data[key] = [data[key]];
       }
-    });
-
-    console.log("Form data:", data);
-
-    fetch("/forms", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        console.log("Response body:", response);
-        console.log("Response status:", response.status);
-        if (!response.ok) {
-          throw new Error(`Network response was not ok ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
+      data[key].push(value);
+    } else {
+      data[key] = value;
+    }
   });
+
+  console.log("Form data:", data);
+
+  fetch("/forms", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      console.log("Response body:", response);
+      console.log("Response status:", response.status);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
