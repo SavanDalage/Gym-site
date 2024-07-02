49a0b13685a4ca38d4e9d21430b06590ac1639fb
SavanDalage <nekomimiwolf@gmail.com>
2024-07-02 18:49:19 +0200

    error handling

--- public/js/formularz.js
+++ public/js/formularz.js
@@ -26,7 +26,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
     });
 
     console.log("Form data:", data);
-    console.log("Form data:", data);
 
     fetch("/forms", {
       method: "POST",
@@ -38,7 +37,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
       .then(async (response) => {
         console.log("Response status:", response.status);
         if (!response.ok) {
-          // Attempt to parse the error message from the response
           const errorMessage = await response.text();
           throw new Error(
             `Network response was not ok: ${response.statusText}, Message: ${errorMessage}`
@@ -48,12 +46,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
       })
       .then((data) => {
         console.log("Success:", data);
-        // Optionally show a success message to the user
         alert("Form submitted successfully!");
       })
       .catch((error) => {
         console.error("Error:", error);
-        // Optionally show an error message to the user
         alert(`Error submitting form: ${error.message}`);
       });
   });
